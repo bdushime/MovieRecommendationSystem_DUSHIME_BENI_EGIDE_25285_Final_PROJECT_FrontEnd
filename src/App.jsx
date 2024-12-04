cb497d61c4dc3a9b2b565d2f8eaae2bf89494455
Dushime Beni <bdushime47@gmail.com>
2024-12-04 15:24:15 +0200

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import './App.css'
+import './App.css'
 import AdminPage from './components/AdminPage'
 import AdminUsersPage from './components/AdminUsersPage'
 import Login from './components/login'
@@ -7,39 +7,21 @@ import MoviesPaginated from './components/MoviesPaginated'
 import PersonalPage from './components/personalPage'
 import Register from './components/Register'
 import { MovieProvider } from '../Context/context'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import UsersPage from './components/UsersPage'
 
 function App() {
-
   return (
     // <MovieProvider>
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/login"
-          element={
-            <Login />
-          }
-        />
-        <Route
-          path="/register"
-          element={
-            <Register />
-          }
-        />
-        <Route
-          path="/admin"
-          element={
-            <AdminPage />
-          }
-        />
-        <Route
-          path="/personal-page"
-          element={
-            <UsersPage />
-          }
-        />
+        {/* Add this route to redirect from root to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/personal-page" element={<UsersPage />} />
       </Routes>
     </BrowserRouter>
     // </MovieProvider>
